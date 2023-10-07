227a4959384dbd460631e678e9f17ff19b03424d
Aayush Gupta <guptaaayush432@gmail.com>
2023-10-07 12:51:28 +0530

    corrected svg string & used assertEq

--- test/unit/DeployMoodNftTest.t.sol
+++ test/unit/DeployMoodNftTest.t.sol
@@ -11,9 +11,9 @@ contract DeployMoodNftTest is Test {
         deployer = new DeployMoodNft();
     }
 
-    function testConvertSvgToUri() public view {
+    function testConvertSvgToUri() public {
         string
-            memory expectedUri = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiB3aWR0aD0iNTAwIiBoZWlnaHQ9IjUwMCI+Cjx0ZXh0IHg9IjAiIHk9IjE1IiBmaWxsPSJibGFjayI+SGkhIFlvdXIgYnJvd3NlciBkZWNvZGVkIHRoaXM8L3RleHQ+Cjwvc3ZnPg==";
+            memory expectedUri = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiB3aWR0aD0iNTAwIiBoZWlnaHQ9IjUwMCI+PHRleHQgeD0iMCIgeT0iMTUiIGZpbGw9ImJsYWNrIj5IaSEgWW91ciBicm93c2VyIGRlY29kZWQgdGhpczwvdGV4dD48L3N2Zz4=";
         string
             memory svg = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="500" height="500"><text x="0" y="15" fill="black">Hi! Your browser decoded this</text></svg>';
 
@@ -22,9 +22,9 @@ contract DeployMoodNftTest is Test {
         console.log(actualUri);
         console.log("hello");
 
-        assert(
-            keccak256(abi.encodePacked(actualUri)) ==
-                keccak256(abi.encodePacked(expectedUri))
+        assertEq(
+            keccak256(abi.encodePacked(actualUri)),
+            keccak256(abi.encodePacked(expectedUri))
         );
     }
 }
